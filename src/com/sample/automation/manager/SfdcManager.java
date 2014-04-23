538a3c79dec851646d271c6683a5edbcadc546fd
drajenda <drajenda@LIN15001293.corp.capgemini.com>
2014-04-23 21:11:14 +0530

    final commit

--- src/com/sample/automation/manager/SfdcManager.java
+++ src/com/sample/automation/manager/SfdcManager.java
@@ -1,13 +1,17 @@
 package com.sample.automation.manager;
 
 import com.sample.automation.utils.MyListener;
+import com.sample.automation.utils.ScreenShots;
 import com.thoughtworks.selenium.Selenium;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+
 import org.openqa.selenium.support.ui.Select;
+
+
 
 
 
@@ -28,7 +32,7 @@ import org.testng.Assert;
 public class SfdcManager {
 	
 	
-	public static void login(WebDriver driver, String getuname,String getpwd,String getName) throws InterruptedException, IOException {
+	public static void login(WebDriver driver, String getuname,String getpwd,String getName) throws Exception {
 	
 
 		EventFiringWebDriver mouseDriver = new EventFiringWebDriver(driver);
@@ -43,28 +47,34 @@ public class SfdcManager {
 	      Runtime.getRuntime().exec(commands);
 		*/
 		
+		ScreenShots sc = new ScreenShots();
+		
+		sc.captureScreen();
+		
 	driver.findElement(By.id("username")).sendKeys(getuname);
 	driver.findElement(By.id("password")).sendKeys(getpwd);
 	
+	sc.captureScreen();
 	driver.findElement(By.xpath(".//*[@id='Login']")).click();
 	
 	Thread.sleep(2000);
-		
+		sc.captureScreen();
 	if(driver.getPageSource().contains(getName))
 {
-			
+			sc.captureScreen();
 		driver.findElement(By.linkText("Opportunities")).click();
 			}
 
 Thread.sleep(3000);
 	
-	
+sc.captureScreen();	
 	
 	
 	
 	//Assert.assertTrue(driver.getPageSource().contains("Opportunities"));
 	
 	String opprtunityName = driver.findElement(By.xpath("//*[@id='bodyCell']/div[3]/div[1]/div/div[2]/table/tbody/tr[2]/th/a")).getText();
+	sc.captureScreen();
 	driver.findElement(By.xpath("//*[@id='bodyCell']/div[3]/div[1]/div/div[2]/table/tbody/tr[2]/th/a")).click();
 	
 	driver.getPageSource().contains(opprtunityName);
@@ -72,13 +82,13 @@ Thread.sleep(3000);
 	Assert.assertTrue(driver.getPageSource().contains("Quotes"));
 	
 	WebElement quote = driver.findElement(By.id("0069000000AJeJ5_RelatedQuoteList"));
-	
+	sc.captureScreen();
 	driver.findElement(By.xpath("//*[@id='0069000000AJeJ5_RelatedQuoteList_body']/table/tbody/tr[2]/th/a")).click();
 	
 	Thread.sleep(2000);
 	
 	String winHandleBefore = driver.getWindowHandle();
-
+sc.captureScreen();
 	driver.findElement(By.xpath("//*[@id='topButtonRow']/input[8]")).click();
 
 	Thread.sleep(4000);
@@ -86,17 +96,17 @@ Thread.sleep(3000);
     driver.switchTo().window(winHandle);
     }
 	
-	
+	sc.captureScreen();
 	driver.getPageSource().contains("Order Detail");
 
 	driver.close();
 
 	Thread.sleep(2000);
 
-
+sc.captureScreen();
 	driver.switchTo().window(winHandleBefore);
 	
-	
+	sc.captureScreen();
 	
 	
 	
